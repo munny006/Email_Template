59a10ed8b342beadb52aec7f5c2b2281eaf34a2c
MunnY <mahmudaaktermunny4@gmail.com>
2021-05-24 21:47:58 +0600

    email

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>Email template</title>
+</head>
+<style type="text/css">
+body{
+	margin:0;
+	background-color: #e4f2f6a1;
+	
+}
+table{
+	border-spacing: 0;
+
+}
+td{
+	padding: 0;
+}
+.wrapper{
+	width: 100%;
+	table-layout: fixed;
+	background-color: #cccccc;
+	padding-bottom: 40px;
+}
+.main{
+	background-color: #ffffff;
+	margin:0 auto;
+	width: 100%;
+	max-width: 600px;
+	border-spacing: 0;
+	font-family: sans-serif;
+	color:black;
+}
+img{
+	border:0;
+}
+.button{
+	background-color: blue;
+	color: white;
+	text-align: center;
+	padding: 12px 20px;
+	text-decoration: none;
+	border-radius: 4px;
+	font-weight: bold;
+	float: center;
+}
+.two-colunm{
+	text-align: center;
+	font-size: 0;
+	padding: 40px 0;
+
+}
+.two-colunm .colunm{
+	width: 100%;
+	max-width: 300px;
+	display: inline-block;
+	vertical-align: top;
+}
+.two-colunm .padding{
+	padding: 20px;
+}
+.two-colunm .content{
+	text-align: left;
+	font-size: 15px;
+	line-height: 20px;
+}
+@media screen and(max-width: 600px;){
+.two-colunm .colunm img{
+	width: 300%!important;
+	max-width: 300%!important;
+
+
+}
+.padding{
+	padding-right: 0!important;
+	padding-left: 0!important;
+}
+}
+</style>
+<body>
+	<center class = "wrapper">
+		<table class="main" width="100%">
+			 <tr>
+			 	<td>
+			 		<table width="100%">
+			 			<tr>
+			 				<td style="background-color: #4980d2; padding: 8px 0 5px; text-align: center;">
+			 					<a href="#"><img src="img/icons8-facebook-50.png" width="30"></a>
+			 					<a href="#"><img src="img/icons8-twitter-50.png" width="30"></a>
+			 					<a href="#"><img src="img/icons8-youtube-music-50.png" width="30"></a>
+			 					<a href="#"><img src="img/icons8-linkedin-50.png" width="30"></a>
+			 					<a href="#"><img src="img/icons8-instagram-24.png" width="30"></a>
+			 				</td>
+			 			</tr>
+			 		</table>
+			 	</td>
+			 </tr>
+
+			 <tr>
+			 	<td>
+			 		<table width="100%">
+			 			<tr>
+			 				<td style="text-align: center; padding: 10px;">
+			 					<a href="https://www.w3schools.com/"><img src="img/icons8-omega-48.png" width="50" alt="Logo" title="Logo"></a>
+			 				</td>
+			 			</tr>
+			 			
+			 		</table>
+			 	</td>
+			 </tr>
+			 <tr>
+			 	<td>
+			 		<a href="#"><img src="img/4dd8992f380071845532c16f21a632fd.jpg" width="100%" alt="banner" style="max-width: 100%;height: 363px;"></a>
+			 	</td>
+			 </tr>
+			 <tr>
+			 	<td style="padding-bottom: 40px;">
+			 		<table width="100%">
+			 			<tr>
+			 				<td style="padding: 15px;text-align: center;">
+			 					<p style="font-size: 20px; font-weight: bold;text-align: center">Html Email Template <strong style="color: blue;">Course</strong></p>
+			 					<p style="font-size: 15px; line-height: 23px;padding: 5px 0 15px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			 					<a href="#" class="button">View Course</a>
+			 				</td>
+			 			</tr>
+			 		</table>
+			 	</td>
+			 </tr>
+			 <tr>
+			 	<td height="5px" style="background-color: blue;">
+			 		
+			 	</td>
+			 </tr>
+			 <tr>
+			 	<td>
+			 		<table width="100%">
+			 			<tr>
+			 				<td class="two-colunm">
+			 					<table class="colunm">
+			 						<tr>
+			 							<td class="padding">
+			 								<table class="content">
+			 									<tr>
+			 										<td>
+			 											<a href=""><img src="img/st.jpg" alt="img" width="260" style="max-width:260";></a>
+			 										</td>
+			 									</tr>
+			 									
+			 								</table>
+			 								
+			 							</td>
+			 						</tr>
+			 						
+			 					</table>
+			 					<table class="colunm">
+			 						<tr>
+			 							<td class="padding">
+			 								<table class="content">
+			 									<tr>
+			 										<td>
+			 											<p style="font-weight: bold;font-size: 17px;">Create Custom Design </p>
+			 											<p style="padding-bottom: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur.</p>
+			 											<a href="#" class="button">Read More</a>
+			 										</td>
+			 									</tr>
+			 									
+			 								</table>
+			 				</td>
+			 			</tr>
+			 			
+			 		</table>
+			 	</td>
+			 </tr>
+			 <tr>
+			 	<td>
+			 		<table width="100%">
+			 			<tr>
+			 				<td style="text-align: center;background-color: blue;color: white;padding: 8px;font-size: 18px;">
+			 					<p style="font-weight: bold;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			 					<p>M_MunnY.</p>
+			 				</td>
+			 			</tr>
+			 			
+			 		</table>
+			 	</td>
+			 </tr>
+
+<tr>
+	<td style="background-color:#efefef;">
+		<table width="100%">
+			<tr>
+				<td style="padding: 30px 10px; text-align:center; color: black;">
+				<a href="https://www.w3schools.com/"><img src="img/icons8-omega-48.png" width="60" alt="Logot" title="Logot"></a>
+				
+				<p>W3MunnY Email</p>
+				<p>123 Street Road,City state 6666</p>
+				<p><a href="mailto:mahmudaaktermunny4@gmail.com" style="color: black;">mahmudaaktermunny4@gmail.com</a></p>
+				<p><a href="tel:9475878574"  style="color: black;">1-4856-45874</a></p>
+			</td>
+			</tr>
+			<tr>
+				<td height="10px" style="background-color: blue;">
+					
+				</td>
+			</tr>
+			
+		</table>
+	</td>
+</tr>
+
+
+
+
+
+
+
+
+
+
+
+			
+		</table>
+		
+	</center>
+
+</body>
+</html>
